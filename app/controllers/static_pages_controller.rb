2315d9b41a214993332d3ead1d7672d02aff9fae
Michele Gravina <Michele.Gravina@student.ncirl.ie>
2015-11-12 22:26:57 +0000

    modified static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,8 @@ class StaticPagesController < ApplicationController
 
   def mycollection
   end
+  
+  def about
+  end
+  
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+	assert_select "title", "MtgCardWallet"
   end
 
   test "should get mycollection" do
     get :mycollection
     assert_response :success
+	assert_select "title", "My Collection"
+  end
+  
+    test "should get about" do
+    get :about
+    assert_response :success
+	assert_select "title", "About mtgCardWallet"
   end
 
 end
